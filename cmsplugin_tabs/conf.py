c54fcb3d048e71760defa5e92f297383c3047875
Venelin Stoykov <venelin@magicsolutions.bg>
2015-12-02 11:30:33 +0200

    Mark default TEMPLATE_CHOICES lables for translation

--- cmsplugin_tabs/conf.py
+++ cmsplugin_tabs/conf.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
 
 
 # If slug in tabs is required or not
@@ -7,8 +8,8 @@ BLANK_SLUG = not getattr(settings, 'TABSPLUGIN_REQUIRE_SLUG', False)
 # Possible templates for rendering the plugin
 # It can look as tabs or as accordion by default
 TEMPLATE_CHOICES = getattr(settings, 'TABSPLUGIN_TEMPLATES', (
-    ('cmsplugin_tabs/tabs.html', 'Tabs'),
-    ('cmsplugin_tabs/accordion.html', 'Accordion'),
+    ('cmsplugin_tabs/tabs.html', _('Tabs')),
+    ('cmsplugin_tabs/accordion.html', _('Accordion')),
 ))
 
 # Default template is the first tempalte
