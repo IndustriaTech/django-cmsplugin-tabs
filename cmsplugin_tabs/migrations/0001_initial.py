a552eb6f8298726b33fba4f8d8dfd6de6c4bccb0
Venelin Stoykov <venelin@magicsolutions.bg>
2015-12-02 11:14:19 +0200

    Use confing in django migrations

--- cmsplugin_tabs/migrations/0001_initial.py
+++ cmsplugin_tabs/migrations/0001_initial.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import migrations, models
 import ckeditor.fields
+from cmsplugin_tabs.conf import BLANK_SLUG, TEMPLATE_CHOICES, DEFAULT_TEMPLATE
 
 
 class Migration(migrations.Migration):
@@ -17,7 +18,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('cmsplugin_ptr', models.OneToOneField(serialize=False, primary_key=True, auto_created=True, to='cms.CMSPlugin', parent_link=True)),
                 ('title', models.CharField(verbose_name='Title', max_length=255)),
-                ('slug', models.SlugField(verbose_name='Slug', default='', max_length=32, blank=True)),
+                ('slug', models.SlugField(verbose_name='Slug', default='', max_length=32, blank=BLANK_SLUG)),
                 ('is_strong', models.BooleanField(default=False, verbose_name='Strong', help_text='When True then label of the tab will be bold')),
                 ('content', ckeditor.fields.RichTextField(blank=True, default='', verbose_name='Content')),
             ],
@@ -31,7 +32,7 @@ class Migration(migrations.Migration):
             name='CMSTabsList',
             fields=[
                 ('cmsplugin_ptr', models.OneToOneField(serialize=False, primary_key=True, auto_created=True, to='cms.CMSPlugin', parent_link=True)),
-                ('template', models.CharField(verbose_name='Template', default='cms/plugins/accordion.html', choices=[('cms/plugins/accordion.html', 'Accordion')], max_length=255)),
+                ('template', models.CharField(verbose_name='Template', default=DEFAULT_TEMPLATE, choices=TEMPLATE_CHOICES, max_length=255)),
             ],
             options={
                 'verbose_name': 'Tab list',
